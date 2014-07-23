b0d494979a1fa2d7eebbfefa2edda24863c517ce
mdt013 <mudit.180@gmail.com>
2014-07-23 15:18:11 +0530

    Alarm Code applied

---
+++ app/src/main/java/com/app/namokarmantra/app/AlarmSound.java
@@ -0,0 +1,75 @@
+package com.app.namokarmantra.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import java.io.IOException;
+
+/**
+ * Created by Mudit on 7/23/2014.
+ */
+public class AlarmSound extends ActionBarActivity{
+    private MediaPlayer player;
+    final Context context=this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.alarmview);
+
+        Button stop = (Button) findViewById(R.id.alarm);
+        stop.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                player.stop();
+                Intent i=new Intent(context,Bye.class);
+                startActivity(i);
+                return false;
+            }
+        });
+
+        play(this, getAlarmSound());
+    }
+
+    private void play(Context context, Uri alert) {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(context, alert);
+            final AudioManager audio = (AudioManager) context
+                    .getSystemService(Context.AUDIO_SERVICE);
+            if (audio.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+                player.setAudioStreamType(AudioManager.STREAM_ALARM);
+                player.prepare();
+                player.start();
+            }
+        } catch (IOException e) {
+            Log.e("Error....","Check code...");
+        }
+    }
+
+    private Uri getAlarmSound() {
+        Uri alertSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alertSound == null) {
+            alertSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            if (alertSound == null) {
+                alertSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+        }
+        return alertSound;
+    }
+
+}
--- app/src/main/java/com/app/namokarmantra/app/MainActivity.java
+++ app/src/main/java/com/app/namokarmantra/app/MainActivity.java
@@ -1,17 +1,46 @@
 package com.app.namokarmantra.app;
 
+import java.util.Calendar;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.MenuItem;
+import android.content.Context;
+import android.content.Intent;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.infojunc.namokarmantra.R;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView myText;
+    Switch mySwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myText =(TextView)findViewById(R.id.textView);
+        mySwitch = (Switch)findViewById(R.id.switch1);
+
+        myText.setText("Turn the app on/off");
+        Calendar cl = Calendar.getInstance();
+        cl.add(Calendar.HOUR_OF_DAY,18);
+
+        Intent i = new Intent(this, AlarmSound.class);
+        PendingIntent pending = PendingIntent.getActivity(this,1235, i, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager alarm = (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+        alarm.set(AlarmManager.RTC_WAKEUP, t.getTimeInMillis(),pending);
+
     }
 
 
